b99eb04b2b060a467808646eaedb34f387ddddb6
rawrower <6372741+rowrawer@users.noreply.github.com>
2020-11-07 22:24:56 +0100

    fixes

--- lastfm.js
+++ lastfm.js
@@ -14,158 +14,162 @@ const options = [
 function debloat() {
 	options.forEach(o => {
 		browser.storage.local.get(o).then(res => {
-			// have to check typeof because of false
-			if (res[o] === true || typeof res[o] === "undefined") {
-				if (o === "artistFirst" || o === "titleSpace") {
-					const elements = document.getElementsByClassName(
-						"chartlist--with-artist"
-					);
-					for (const e of elements) {
-						if (!e.classList.contains("chartlist--with-bar")) {
-							for (const row of e.getElementsByClassName("chartlist-row")) {
-								// contains(o) to monitor class list to not fall into endless loop
-								if (!row.classList.contains(o)) {
-									const artist = row.getElementsByClassName(
-										"chartlist-artist"
-									)[0];
-									const songName = row.getElementsByClassName(
-										"chartlist-name"
-									)[0];
+			if (res[o] === false) return;
+			if (o === "artistFirst" || o === "titleSpace") {
+				const elements = document.getElementsByClassName(
+					"chartlist--with-artist"
+				);
+				for (const e of elements) {
+					if (!e.classList.contains("chartlist--with-bar")) {
+						for (const row of e.getElementsByClassName("chartlist-row")) {
+							// contains(o) to monitor class list to not fall into endless loop
+							if (row.classList.contains(o)) return;
+							const artist = row.getElementsByClassName("chartlist-artist")[0];
+							const songName = row.getElementsByClassName("chartlist-name")[0];
 
-									if (o === "titleSpace") {
-										artist.style.flexGrow = "0";
-										artist.style.flexShrink = "0";
-										artist.style.width = "inherit";
-										songName.style.width = "inherit";
-									}
-
-									if (o === "artistFirst") {
-										row.insertBefore(artist, songName);
-
-										const emDash = document.createElement("span");
-										emDash.textContent = "—";
-										emDash.className = "emDash";
-
-										if (row.getElementsByClassName("emDash").length === 0)
-											// only insert if necessary
-											row.insertBefore(emDash, songName);
-									}
-									row.classList.add(o); // class list monitoring purposes
-								}
+							if (o === "titleSpace") {
+								artist.style.flexGrow = "0";
+								artist.style.flexShrink = "0";
+								artist.style.width = "inherit";
+								songName.style.width = "inherit";
 							}
-						} else if (e.classList.contains("chartlist--with-bar")) {
-							for (const row of e.getElementsByClassName("chartlist-row")) {
-								if (!row.classList.contains(o)) {
-									if (o === "artistFirst") {
-										const artist = row.getElementsByClassName(
-											"chartlist-artist"
-										)[0];
-										const songName = row.getElementsByClassName(
-											"chartlist-name"
-										)[0];
 
-										artist.style.flexGrow = "0";
-										artist.style.flexShrink = "0";
-										artist.style.position = "static";
-										artist.style.marginTop = "inherit";
-										artist.style.marginRight = "7.5px";
-										songName.style.marginBottom = "inherit";
+							if (o === "artistFirst") {
+								row.insertBefore(artist, songName);
 
-										row.insertBefore(artist, songName);
+								const emDash = document.createElement("span");
+								emDash.textContent = "—";
+								emDash.className = "emDash";
 
-										const emDash = document.createElement("span");
-										emDash.textContent = "—";
-										emDash.className = "emDash";
+								if (row.getElementsByClassName("emDash").length === 0)
+									// only insert if necessary
+									row.insertBefore(emDash, songName);
+							}
+							row.classList.add(o); // class list monitoring purposes
+						}
+					} else if (e.classList.contains("chartlist--with-bar")) {
+						for (const row of e.getElementsByClassName("chartlist-row")) {
+							if (row.classList.contains(o)) return;
+							if (o === "artistFirst") {
+								const artist = row.getElementsByClassName(
+									"chartlist-artist"
+								)[0];
+								const songName = row.getElementsByClassName(
+									"chartlist-name"
+								)[0];
 
-										if (row.getElementsByClassName("emDash").length === 0)
-											row.insertBefore(emDash, songName);
-										row.classList.add(o);
-									}
-								}
+								artist.style.flexGrow = "0";
+								artist.style.flexShrink = "0";
+								artist.style.position = "static";
+								artist.style.marginTop = "inherit";
+								artist.style.marginRight = "7.5px";
+								songName.style.marginBottom = "inherit";
+
+								row.insertBefore(artist, songName);
+
+								const emDash = document.createElement("span");
+								emDash.textContent = "—";
+								emDash.className = "emDash";
+
+								if (row.getElementsByClassName("emDash").length === 0)
+									row.insertBefore(emDash, songName);
+								row.classList.add(o);
 							}
 						}
 					}
 				}
+			}
 
-				if (o === "squareAvatars") {
-					// horrible workaround for css pseudoclass
-					const rule = ".avatar::after { border-radius: 0px !important; }";
-					if (document.styleSheets[0].cssRules[0].cssText !== rule)
-						document.styleSheets[0].insertRule(rule, 0);
+			if (o === "squareAvatars") {
+				// horrible workaround for css pseudoclass
+				const rule = ".avatar::after { border-radius: 0px !important; }";
+				if (document.styleSheets[0].cssRules[0].cssText !== rule)
+					document.styleSheets[0].insertRule(rule, 0);
 
-					const elements = document.getElementsByClassName("avatar");
+				const elements = document.getElementsByClassName("avatar");
 
-					for (const e of elements) {
-						if (!e.classList.contains(o)) {
-							e.classList.add(o);
-							const img = e.getElementsByTagName("img");
-							for (const i of img) {
-								i.style.borderRadius = "0";
-							}
-						}
-					}
+				for (const e of elements) {
+					if (e.classList.contains(o)) return;
+
+					e.classList.add(o);
+					const img = e.getElementsByTagName("img");
+					for (const i of img) i.style.borderRadius = "0";
 				}
+			}
 
-				if (
-					o === "compactCharts" ||
-					o === "barColor" ||
-					o === "barFontInvert" ||
-					o === "fontWeight" ||
-					o === "scrobbleText"
-				) {
-					const elements = document.getElementsByClassName("chartlist-row");
-					for (const e of elements) {
-						if (!e.classList.contains(o)) {
-							if (o === "compactCharts") {
-								e.style.paddingTop = "4px";
-								e.style.paddingBottom = "4px";
-								e.style.minHeight = "0px";
-							}
+			if (
+				o === "compactCharts" ||
+				o === "barColor" ||
+				o === "barFontInvert" ||
+				o === "fontWeight" ||
+				o === "scrobbleText"
+			) {
+				const elements = document.getElementsByClassName("chartlist-row");
+				for (const e of elements) {
+					if (e.classList.contains(o)) return;
+					if (o === "compactCharts") {
+						e.style.paddingTop = "4px";
+						e.style.paddingBottom = "4px";
+						e.style.minHeight = "0px";
+					}
 
-							if (e.getElementsByClassName("chartlist-bar").length > 0) {
-								if (o === "barColor") {
-									browser.storage.local.get("barColorPicker").then(color => {
-										e.getElementsByClassName(
+					if (e.getElementsByClassName("chartlist-bar").length > 0) {
+						if (o === "barColor")
+							browser.storage.local
+								.get("barColorPicker")
+								.then(
+									color =>
+										(e.getElementsByClassName(
 											"chartlist-count-bar-slug"
 										)[0].style.backgroundColor =
-											color.barColorPicker || "#b90000";
-									});
-								}
+											color.barColorPicker || "#b90000")
+								);
 
-								if (o === "barFontInvert") {
-									e.getElementsByClassName(
-										"chartlist-count-bar-value"
-									)[0].style.color = "#ffffff";
-								}
+						if (o === "barFontInvert") {
+							e.getElementsByClassName(
+								"chartlist-count-bar-value"
+							)[0].style.color = "#ffffff";
 
-								if (o === "scrobbleText") {
-									e.getElementsByClassName("stat-name")[0].style.display =
-										"none";
-								}
-							}
-
-							if (o === "fontWeight")
-								e.getElementsByClassName("chartlist-name")[0].style.fontWeight =
-									"inherit";
-
-							e.classList.add(o);
+							// white on white workaround
+							browser.storage.local.get("barColorPicker").then(
+								color =>
+									(e.getElementsByClassName(
+										"chartlist-count-bar-link"
+									)[0].style.backgroundColor =
+										color.barColorPicker ||
+										// no clue why getComputedStyle has to be used here
+										window
+											.getComputedStyle(
+												e.getElementsByClassName("chartlist-count-bar-slug")[0]
+											)
+											.getPropertyValue("background-color"))
+							);
 						}
+
+						if (o === "scrobbleText")
+							e.getElementsByClassName("stat-name")[0].style.display = "none";
 					}
+
+					if (o === "fontWeight")
+						e.getElementsByClassName("chartlist-name")[0].style.fontWeight =
+							"inherit";
+
+					e.classList.add(o);
 				}
+			}
 
-				if (o === "compactArtistHeader") {
-					let header = document.getElementsByClassName("header-new-content");
-					if (header.length > 0) [header] = header;
+			if (o === "compactArtistHeader") {
+				let header = document.getElementsByClassName("header-new-content");
+				if (header.length > 0) [header] = header;
 
-					if (header && !header.classList.contains(o)) {
-						header.getElementsByClassName(
-							"artist-header-featured-items"
-						)[0].style.marginTop = "16px";
+				if (header && !header.classList.contains(o)) {
+					header.getElementsByClassName(
+						"artist-header-featured-items"
+					)[0].style.marginTop = "16px";
 
-						header.style.paddingTop = "16px";
-						header.style.paddingBottom = "16px";
-						header.classList.add(o);
-					}
+					header.style.paddingTop = "16px";
+					header.style.paddingBottom = "16px";
+					header.classList.add(o);
 				}
 			}
 		});
--- options.js
+++ options.js
@@ -1,22 +1,19 @@
 const options = document.getElementsByClassName("option");
 
 function saveOptions(o) {
-	if (o.id === "barColorPicker") {
+	if (o.id === "barColorPicker")
 		browser.storage.local.set({
 			[o.id]: document.getElementById(o.id).value
 		});
-	} else {
+	else
 		browser.storage.local.set({
 			[o.id]: document.getElementById(o.id).checked
 		});
-	}
-	if (o.id === "barColor") {
-		if (o.checked) {
-			document.getElementById("barColorPicker").disabled = false;
-		} else {
-			document.getElementById("barColorPicker").disabled = true;
-		}
-	}
+
+	if (o.id === "barColor")
+		o.checked
+			? (document.getElementById("barColorPicker").disabled = false)
+			: (document.getElementById("barColorPicker").disabled = true);
 }
 
 function restoreOptions() {
@@ -24,11 +21,10 @@ function restoreOptions() {
 		browser.storage.local.get(o.id).then(res => {
 			// have to check typeof because of false
 			if (typeof res[o.id] !== "undefined") {
-				if (o.id === "barColorPicker") {
-					document.getElementById(o.id).value = res[o.id];
-				} else {
-					document.getElementById(o.id).checked = res[o.id];
-				}
+				o.id === "barColorPicker"
+					? (document.getElementById(o.id).value = res[o.id])
+					: (document.getElementById(o.id).checked = res[o.id]);
+
 				if (o.id === "barColor" && res[o.id] === false)
 					document.getElementById("barColorPicker").disabled = true;
 			} else {
@@ -50,6 +46,4 @@ for (const o of options) {
 // i18n of options menu
 const _ = browser.i18n.getMessage;
 const labels = document.getElementsByTagName("label");
-for (const l of labels) {
-	l.textContent = _(l.htmlFor);
-}
+for (const l of labels) l.textContent = _(l.htmlFor);
